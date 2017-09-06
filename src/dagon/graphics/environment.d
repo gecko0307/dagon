4575ab95cec3c383f8876fac3712fe15258ab727
Timur Gafarov <gecko0307@gmail.com>
2017-09-06 19:17:12 +0400

    Sky colors in Environment

--- src/dagon/graphics/environment.d
+++ src/dagon/graphics/environment.d
@@ -22,6 +22,17 @@ class Environment: Owner
     Color4f sunZenithColor = Color4f(0.7, 0.7, 0.6, 1.0);
     Color4f sunHorizonColor = Color4f(0.8, 0.3, 0.0, 1.0);
     Quaternionf sunRotation;
+    
+    Color4f skyZenithColor = Color4f(0.5, 0.7, 1.0, 1.0);
+    Color4f skyHorizonColor = Color4f(0.9, 1.0, 1.0, 1.0);
+    
+    Color4f skyZenithColorAtMidday = Color4f(0.5, 0.7, 1.0, 1.0);
+    Color4f skyZenithColorAtSunset = Color4f(0.2, 0.4, 0.5, 1.0);
+    Color4f skyZenithColorAtNight = Color4f(0.0, 0.0, 0.2, 1.0);
+    
+    Color4f skyHorizonColorAtMidday = Color4f(0.9, 1.0, 1.0, 1.0);
+    Color4f skyHorizonColorAtSunset = Color4f(0.8, 0.3, 0.1, 1.0);
+    Color4f skyHorizonColorAtNight = Color4f(0.0, 0.0, 0.0, 1.0);
 
     // TODO: shadow map (CSM)
 
@@ -32,6 +43,15 @@ class Environment: Owner
         sunRotation = rotationQuaternion(Axis.x, degtorad(-45.0f)); //Quaternionf.identity;
     }
     
+    void update(double dt)
+    {
+        skyZenithColor = lerpColorsBySunAngle(skyZenithColorAtMidday, skyZenithColorAtSunset, skyZenithColorAtNight);
+        skyHorizonColor = lerpColorsBySunAngle(skyHorizonColorAtMidday, skyHorizonColorAtSunset, skyHorizonColorAtNight);
+        backgroundColor = skyZenithColor;
+        fogColor = skyHorizonColor;
+        ambientConstant = Color4f(0.0f, 0.05f, 0.05f) + skyZenithColor * 0.3f;
+    }
+    
     Vector3f sunDirection()
     {
         return sunRotation.rotate(Vector3f(0, 0, 1));
@@ -39,19 +59,19 @@ class Environment: Owner
 
     Color4f sunColor()
     {
-        return lerpColorsBySunAngle(sunZenithColor, sunHorizonColor);
+        return lerpColorsBySunAngle(sunZenithColor, sunHorizonColor, Color4f(0.0f, 0.0f, 0.0f, 1.0f));
     }
     
-    Color4f lerpColorsBySunAngle(Color4f atZenith, Color4f atHorizon)
+    Color4f lerpColorsBySunAngle(Color4f atZenith, Color4f atHorizon, Color4f atNightSide)
     {
         float s = dot(sunDirection, Vector3f(0.0, 1.0, 0.0));
         Vector3f sunColor;
         if (s < 0.01f)
-            sunColor = Vector3f(0.0f, 0.0f, 0.0f);
+            sunColor = atNightSide;
         else if (s < 0.08f)
         {
             sunColor = lerp(atHorizon, atZenith, s);
-            sunColor = lerp(sunColor, Vector3f(0.0f, 0.0f, 0.0f), (0.07f - (s - 0.01f)) / 0.07f);
+            sunColor = lerp(sunColor, Vector3f(atNightSide), (0.07f - (s - 0.01f)) / 0.07f);
         }
         else
             sunColor = lerp(Vector3f(atHorizon), Vector3f(atZenith), s);
