a4c10d06236a49be99fed8f8d74a1d9075eb942a
TimurGafarov <gt@smarthead.ru>
2018-08-17 18:31:33 +0300

    Import fix

--- src/dagon/logics/charactercontroller.d
+++ src/dagon/logics/charactercontroller.d
@@ -25,7 +25,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-module charactercontroller;
+module dagon.logics.charactercontroller;
 
 import std.math;
 
