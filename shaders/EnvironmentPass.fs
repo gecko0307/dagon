1e131813cfb16d4fecab8e1fa561e02e10e43502
TimurGafarov <gt@smarthead.ru>
2018-10-19 13:28:39 +0300

    Better SSAO

--- shaders/EnvironmentPass.fs
+++ shaders/EnvironmentPass.fs
@@ -212,6 +212,70 @@ float luminance(vec3 color)
     );
 }
 
+
+#define SAMPLES 16
+#define INTENSITY 1.0
+#define SCALE 2.5
+#define BIAS 0.05
+#define SAMPLE_RAD 0.1
+//0.02
+#define MAX_DISTANCE 0.07
+#define MOD3 vec3(0.1031, 0.11369, 0.13787)
+
+float hash12(vec2 p)
+{
+	vec3 p3  = fract(vec3(p.xyx) * MOD3);
+    p3 += dot(p3, p3.yzx + 19.19);
+    return fract((p3.x + p3.y) * p3.z);
+}
+
+vec2 hash22(vec2 p)
+{
+	vec3 p3 = fract(vec3(p.xyx) * MOD3);
+    p3 += dot(p3, p3.yzx+19.19);
+    return fract(vec2((p3.x + p3.y)*p3.z, (p3.x+p3.z)*p3.y));
+}
+
+vec2 getRandom(vec2 uv) {
+    return normalize(hash22(uv*126.1231) * 2.0 - 1.0);
+}
+
+float doAmbientOcclusion(in vec2 tcoord,in vec2 uv, in vec3 p, in vec3 cnorm)
+{
+	vec3 pos = texture(positionBuffer, tcoord + uv).xyz;
+    vec3 diff = pos - p;
+    float l = length(diff);
+    vec3 v = diff/l;
+    float d = l*SCALE;
+    float ao = max(0.0,dot(cnorm,v)-BIAS)*(1.0/(1.0+d));
+    //ao *= smoothstep(MAX_DISTANCE, MAX_DISTANCE * 0.5, l);
+    return ao;
+
+}
+
+float spiralAO(vec2 uv, vec3 p, vec3 n, float rad)
+{
+    float goldenAngle = 2.4;
+    float ao = 0.0;
+    float inv = 1.0 / float(SAMPLES);
+    float radius = 0.0;
+
+    float rotatePhase = hash12( uv*100.0 ) * 6.28;
+    float rStep = inv * rad;
+    vec2 spiralUV;
+
+    for (int i = 0; i < SAMPLES; i++) {
+        spiralUV.x = sin(rotatePhase);
+        spiralUV.y = cos(rotatePhase);
+        radius += rStep;
+        ao += doAmbientOcclusion(uv, spiralUV * radius, p, n);
+        rotatePhase += goldenAngle;
+    }
+    ao *= inv;
+    return ao;
+}
+
+
 layout(location = 0) out vec4 frag_color;
 layout(location = 1) out vec4 frag_luminance;
 
@@ -266,6 +330,7 @@ void main()
     if (enableSSAO)
     {
         occlusion = 0.0;
+		/*
         vec3 rvec = noise(texCoord);
         vec3 tangent = normalize(rvec - N * dot(rvec, N));
         vec3 bitangent = cross(tangent, N);
@@ -288,7 +353,15 @@ void main()
             float attenuation = 1.0 / (1.0 + dist);
             occlusion += intensity * attenuation;
         }
-        occlusion = clamp(1.0 - occlusion, 0.0, 1.0);
+		*/
+		
+        vec3 p = eyePos; //texture(positionBuffer, sampleUV).xyz; //getPosition(uv);
+        vec3 n = N; //getNormal(uv);
+        float ao = 0.0;
+        float rad = SAMPLE_RAD/p.z;
+        ao = spiralAO(texCoord, p, n, rad);
+        ao = 1.0 - ao * INTENSITY;
+        occlusion = ao * ao; //clamp(1.0 - occlusion, 0.0, 1.0);
     }
 
     vec3 radiance = vec3(0.0, 0.0, 0.0);
