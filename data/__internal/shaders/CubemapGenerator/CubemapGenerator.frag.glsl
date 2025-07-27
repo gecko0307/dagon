272cd6353f0fe5f2561b8f27197caa4b2648ed7f
Timur Gafarov <gecko0307@gmail.com>
2025-07-27 15:04:47 +0300

    Fix envMapEquirect

--- data/__internal/shaders/CubemapGenerator/CubemapGenerator.frag.glsl
+++ data/__internal/shaders/CubemapGenerator/CubemapGenerator.frag.glsl
@@ -1,6 +1,7 @@
 #version 400 core
 
 #define PI 3.14159265359
+const float PI2 = PI * 2.0;
 
 in vec2 texCoord;
 
@@ -9,18 +10,13 @@ uniform mat4 pixelToWorldMatrix;
 
 layout(location = 0) out vec4 fragColor;
 
-vec2 equirectProj(vec3 dir)
-{
-    float phi = acos(dir.y);
-    float theta = atan(dir.x, dir.z) + PI;
-    return vec2(theta / (PI * 2.0), phi / PI);
-}
+#include <envMapEquirect.glsl>
 
 void main()
 {
     vec2 ndc = vec2(texCoord.x, 1.0 - texCoord.y) * 2.0 - 1.0;
     vec3 ray = normalize(vec3(ndc, 1.0f));
     vec3 rayWorld = (pixelToWorldMatrix * vec4(ray, 0.0f)).xyz;
-    vec2 sampleTexCoord = equirectProj(rayWorld);
+    vec2 sampleTexCoord = envMapEquirect(rayWorld);
     fragColor = texture(envmap, sampleTexCoord);
 }
--- data/__internal/shaders/include/envMapEquirect.glsl
+++ data/__internal/shaders/include/envMapEquirect.glsl
@@ -1,6 +1,6 @@
-vec2 envMapEquirect(in vec3 dir)
+vec2 envMapEquirect(vec3 dir)
 {
-    float phi = acos(dir.y);
-    float theta = atan(dir.x, dir.z) + PI;
-    return vec2(1.0 - theta / PI2, phi / PI);
+    float u = 1.0 - (atan(dir.x, dir.z) / PI2 + 0.5);
+    float v = acos(dir.y) / PI;
+    return vec2(u, v);
 }
