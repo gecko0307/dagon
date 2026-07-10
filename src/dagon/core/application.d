4ab5a1c7a3a790b9a63179e295dcfcf2db9e0815
Timur Gafarov <gecko0307@gmail.com>
2026-07-10 23:59:40 +0300

    Update version

--- src/dagon/core/application.d
+++ src/dagon/core/application.d
@@ -102,10 +102,10 @@ struct DagonVersion
 }
 
 /// Engine version number as a read-only structure.
-immutable DagonVersion dagonVersion = DagonVersion(1, 3, 0);
+immutable DagonVersion dagonVersion = DagonVersion(1, 3, 1);
 
 /// Engine version number as a read-only string.
-immutable string dagonVersionString = "1.3.0";
+immutable string dagonVersionString = "1.3.1";
 
 /// Internally reserved event codes.
 enum DagonEvent
